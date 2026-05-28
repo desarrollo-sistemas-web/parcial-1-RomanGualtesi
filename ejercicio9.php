ca9cbacb1e1280ba568c37c14f25ccbab3462908
joaaquinelia <joaquinelia294@gmail.com>
2026-05-28 20:23:35 -0300

    entrego

--- ejercicio9.php
+++ ejercicio9.php
@@ -3,6 +3,8 @@
 //Ejercicio 9: Crea una función llamada generarTablaMultiplicar que tome un número entero como parámetro y mostrar los resultados (del 1 al 10) uno debajo del otro por pantalla.
 //Complejidad: BAJA (1 punto)
 
-function generarTablaMultiplicar($num) {
-    // Tu código aquí
+function generarTablaMultiplicar(int $num) {
+    for ($i=1; $i <= 10; $i++) { 
+        echo $num * $i . "<br>";
+    }
 }
